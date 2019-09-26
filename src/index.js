48f66ed96a0ab76fdaa27af503a5b9ae41ea18f2
szczepcio95 <jszczepanik@deloittece.com>
2019-09-26 13:52:45 +0200

    Setting up first component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class DummyComponent extends Component {
+export default class Translateable extends React.Component {
 
     render () {
 
